ba643c5c67874ac8bb879e7d26300c1841ff4f96
ruroru <111705692+ruroru@users.noreply.github.com>
2025-06-14 13:44:33 +0000

    Stepping project version for the next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/surykatka "1.2.0"
+(defproject org.clojars.jj/surykatka "1.2.1-SNAPSHOT"
   :description "A Clojure library that identifies file type based on magic numbers."
 
   :url "https://github.com/ruroru/surykatka"
